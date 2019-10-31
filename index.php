7af58aa2bf77f71da375f1a9eeb250d3b184f655
Thibault Trembleau <trembleau.thibault@gmail.com>
2019-10-31 16:11:15 +0100

    Initialisation du site

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="style/style1.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+<header>
+	<div class="header-menu">
+		<div class="menu-global">
+			<div class="menu-global-inner">
+				<div class="logo-main">
+					<div class="logo-inner">
+						<span>IUT Dijon</span>
+					</div>
+				</div>
+				<div class="menu-main">
+					<ul class="mid-menu">
+						<li><a href="#">MenuUN</a></li>
+						<li><a href="#">MenuDEUX</a></li>
+						<li><a href="#">MenuTROIS</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+	<main class="content">
+		<div class="content-home">
+			<div class="content-home-box">
+				<div class="home-box-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris interdum, ante at consectetur rhoncus, risus augue vulputate sapien, vel feugiat urna magna id velit. Nam in dignissim urna, sit amet rhoncus leo. Duis risus enim, tempor vitae egestas quis, placerat nec enim. Praesent sem quam, iaculis eget lectus eu, tempor aliquet libero. In mollis mollis mi, at faucibus purus placerat in. Aliquam sit amet neque ullamcorper, lobortis justo sit amet, faucibus augue. Quisque bibendum augue ipsum, et aliquet elit bibendum ac. Quisque pulvinar nulla nisl, ac imperdiet eros tempus vel. Pellentesque semper metus eget porta pharetra. In pulvinar ligula enim, sed imperdiet arcu rutrum quis. Phasellus a quam fermentum, auctor lectus quis, lobortis neque. Duis dignissim eros vitae turpis aliquam ullamcorper.
+				</div>
+			</div>
+		</div>
+		<div class="content-home">
+			<div class="content-home-box">
+				<div class="home-box-2">
+				Donec eu arcu ut arcu pharetra pharetra. Maecenas purus justo, cursus vitae maximus eu, fringilla semper massa. Phasellus vel mollis lectus. Morbi sapien quam, consequat eget tristique nec, dignissim vel sapien. Nullam urna eros, gravida non sollicitudin vel, euismod ut elit. Ut erat neque, eleifend quis aliquet a, euismod ut ex. Praesent risus nibh, scelerisque ut interdum in, interdum sollicitudin enim. Integer non eleifend dui. Suspendisse id dignissim odio. Morbi tempor dapibus quam et maximus. Nunc maximus fringilla tellus et porta. Maecenas massa dui, accumsan eu ultricies condimentum, scelerisque convallis ex. Aliquam erat volutpat.
+				</div>
+			</div>
+		</div>
+	</main>
+<footer class="main-footer">
+</footer>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="script/script1.js"></script>
+</body>
+</html>
