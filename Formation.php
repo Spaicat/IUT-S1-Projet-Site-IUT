db8111972a422e4b99b3093d34aba93ac5acb11c
Spaicat <55053185+Spaicat@users.noreply.github.com>
2019-12-11 00:14:23 +0100

    Ajout contenu formation

--- Formation.php
+++ Formation.php
@@ -5,7 +5,184 @@
 <body>
 <header>
 	<?php include 'include/header-1.php'?>
+	<div class="second-title">
+		<div class="second-title-inner">
+			<h2>Formation</h2>
+		</div>
+	</div>
 </header>
+
+<main class="main-content">
+	<div class="second-content">
+		<div class="content-part">
+			<h3>Répartition des enseignements</h3>
+			<h4>Semestre 1</h4>
+			<table>
+				<tr>
+					<th>Catégorie</th>
+					<th>Discipline</th>
+					<th>Volume horaire</th>
+					<th>Coefficients</th>
+				</tr>
+				<tr>
+					<td rowspan="6">Bases de l'informatique</td>
+					<td>Introduction aux systèmes informatiques</td>
+					<td>54</td>
+					<td>3.5</td>
+				</tr>
+				<tr>
+					<td>Introduction à l'algorithme et à la programmation</td>
+					<td>59</td>
+					<td>3.5</td>
+				</tr>
+				<tr>
+					<td>Structures de données et algorithmes fondamentaux</td>
+					<td>41</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Introduction aux bases de données</td>
+					<td>58</td>
+					<td>3.5</td>
+				</tr>
+				<tr>
+					<td>Conception de documents et d'interfaces numériques</td>
+					<td>44</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Projet tutoré</td>
+					<td>0 (Travail personnel)</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td rowspan="6">Bases de culture scientifique sociale et humaine</td>
+					<td>Mathématiques discrètes</td>
+					<td>42</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Algèbre linéaire</td>
+					<td>30</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td>Environnement économique</td>
+					<td>30</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td>Fonctionnement des organisations</td>
+					<td>42</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Fondamentaux de la communication</td>
+					<td>24</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td>Anglais et informatique</td>
+					<td>24</td>
+					<td>1.5</td>
+				</tr>
+			</table>
+
+			<h4>Semestre 2</h4>
+			<table>
+				<tr>
+					<th>Catégorie</th>
+					<th>Discipline</th>
+					<th>Volume horaire</th>
+					<th>Coefficients</th>
+				</tr>
+				<tr>
+					<td rowspan="7">Approfondissements en informatique</td>
+					<td>Architecture et programmation des mécanismes de base d'un système informatique</td>
+					<td>28</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td>Architecture des réseaux</td>
+					<td>28</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td>Bases de la programmation orientée objet (POO)</td>
+					<td>58</td>
+					<td>3.5</td>
+				</tr>
+				<tr>
+					<td>Bases de la conception objet</td>
+					<td>42</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Introduction aux IHM</td>
+					<td>41</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Programmation et administration des bases de données</td>
+					<td>42</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Projet tutoré</td>
+					<td>0 (Travail personnel)</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td rowspan="7">Approfondissements en culture, scientifique, sociale et humaine</td>
+					<td>Graphes et langages</td>
+					<td>44</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>Analyse et méthodes numériques</td>
+					<td>30</td>
+					<td>2</td>
+				</tr>
+				<tr>
+					<td>Environnement comptable, financier, juridique et social</td>
+					<td>42</td>
+					<td>3</td>
+				</tr>
+				<tr>
+					<td>Gestion de projet informatique</td>
+					<td>30</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td>Communication, information et argumentation</td>
+					<td>24</td>
+					<td>1.5</td>
+				</tr>
+				<tr>
+					<td>Communiquer en anglais</td>
+					<td>38</td>
+					<td>2.5</td>
+				</tr>
+				<tr>
+					<td>PPP</td>
+					<td>24</td>
+					<td>1</td>
+				</tr>
+			</table>
+		</div>
+		<div class="content-part">
+			<h3>Projets</h3>
+			<p>
+				Au cours des deux ans du DUT Informatique différents projets en groupe doivent être réalisés.
+				Ils permettent de développer ses compétences en informatiques ainsi que d'autres compétences comme la gestion de projets et le travail en groupe.
+			</p>
+		</div>
+		<div class="content-part">
+			<h3>Commentaires d'étudiants</h3>
+			<p>Qui parles le mieux du DUT que les étudiants eux mêmes : </p>
+		</div>
+	</div>
+</main>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="script/script1.js"></script>
--- Prerequis.php
+++ Prerequis.php
@@ -7,9 +7,7 @@
 	<?php include 'include/header-1.php'?>
 	<div class="second-title">
 		<div class="second-title-inner">
-			<h2>
-				Prérequis
-			</h2>
+			<h2>Prérequis</h2>
 		</div>
 	</div>
 </header>
