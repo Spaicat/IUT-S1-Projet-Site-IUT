b4f23506840bc596c743dcdaaf123a3a44741e7a
Thibault Trembleau <trembleau.thibault@gmail.com>
2020-01-18 21:48:56 +0100

    disposition du contenu modifié

--- Prerequis.php
+++ Prerequis.php
@@ -13,99 +13,139 @@
 </header>
 
 <main class="main-content">
-	<div class="second-content">
 		<div class="content-part">
 			<div class="title-content">
-				<h2>Les prérequis du DUT Informatique</h2>
+				<div>
+					<h2>Les prérequis du DUT Informatique</h2>
+				</div>
 			</div>
-			<div class="container-content">
-				<p>
-					Comme dans toutes études supérieures le DUT Informatique nécessite quelques prérequis, mais il faut garder en tête que lorsque vous arrivez à l'IUT, vous venez pour apprendre. Il ne faut pas être un pro en programmation pour accéder à ce DUT.
-				</p>
+			<div class="section-content">
+				<ul class="menu-content">
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+				</ul>
+				<div class="container-content">
+					<p>
+						Comme dans toutes études supérieures le DUT Informatique nécessite quelques prérequis, mais il faut garder en tête que lorsque vous arrivez à l'IUT, vous venez pour apprendre. Il ne faut pas être un pro en programmation pour accéder à ce DUT.
+					</p>
+				</div>
 			</div>
 		</div>
 		<div class="content-part">
-			<div class="container-content">
-				<p>
-					Pour accéder au DUT Informatique il faut être titulaire d'un baccalauréat. Les bacheliers les plus recherchés en informatique viennent de S ou STI2D, mais d'autres BAC sont évidemment acceptés. L'admission se faisant sur dossier, un bon parcours au lycée aide à être accepté plus facilement.
-				</p>
+			<div class="title-content-global">
+				<div class="title-content">
+					<div>
+						<h2>Le baccalauréat</h2>
+					</div>
+				</div>
 			</div>
-			<div class="title-content">
-				<h2>Le baccalauréat</h2>
+			<div class="section-content">
+				<ul class="menu-content">
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+				</ul>
+				<div class="container-content">
+					<p>
+						Pour accéder au DUT Informatique il faut être titulaire d'un baccalauréat. Les bacheliers les plus recherchés en informatique viennent de S ou STI2D, mais d'autres BAC sont évidemment acceptés. L'admission se faisant sur dossier, un bon parcours au lycée aide à être accepté plus facilement.
+					</p>
+				</div>
 			</div>
 		</div>
 		<div class="content-part">
-			<div class="title-content">
-				<h2>Les attendus</h2>
+			<div class="title-content-global">
+				<div class="title-content">
+					<div>
+						<h2>Les attendus</h2>
+					</div>
+				</div>
 			</div>
-			<div class="container-content">
-				<p>
-					Bien que les étudiants viennent dans ce DUT pour apprendre, quelques connaissances sont nécessaires.
-					<ul>
-						<li>Un bon niveau en mathématique (n'ayez pas peur, cela ne déterminera pas votre DUT, vous devrez simplement donner un peu plus d'efforts)</li>
-						<li>Une expression française correcte et une bonne maîtrise de l'anglais</li>
-						<li>Ouverture d'esprit, rigueur, autonomie</li>
-						<li>Logique, esprit d'analyse et de synthèse</li>
-					</ul>
-				</p>
-			</div>
-		</div>
-		<div class="content-part">
-			<div class="container-content">
-				<h3>Parcoursup</h3>
-					<p>Pour accéder au DUT Informatique vous devrez premièrement accéder à Parcoursup. Si vous êtes en établissement scolaire vos professeurs vous accompagneront dans la démarche d'inscription qui ouvre le 20 décembre.</p>
-				
-					<p>Le principe de Parcoursup est de formuler des voeux dans différents établissements qui analyseront par la suite votre dossier. Suite à votre inscription différentes démarches sont à effectuer..</p>
-					<div class="card-menu">
-						<div class="card-content card-phase-1" onclick="buttonAppearText(1)">
-							<span class="card-title">22 janvier - 12 mars : Formulation des voeux</span>
-						</div>
-						<div class="card-content card-phase-2" onclick="buttonAppearText(2)">
-							<span class="card-title">2 avril : Limite de la phase d'admission</span>
-						</div>
-						<div class="card-content card-phase-3" onclick="buttonAppearText(3)">
-							<span class="card-title">19 mai - 17 juillet</span>
-						</div>
-						<div class="card-content card-phase-4" onclick="buttonAppearText(4)">
-							<span class="card-title">Début juillet</span>
-						</div>
-						<div class="card-content card-phase-5" onclick="buttonAppearText(5)">
-							<span class="card-title">Fin juin - 11 septembre</span>
-						</div>
-					</div>
-
-					<div class="card-text text-phase-1">
-						<p>Cette période paraît longue mais vous devez impérativement réflechir aux différents voeux que vous allez faire, vous en avez une limite de 10. Vous n'êtes pas seuls à postuler, plus vous serez rapide et sûr de vous, mieux se déroulera vos démarches.</p>
-					</div>
-					<div class="card-text text-phase-2">
-						<p>Il s'agît de la date butoire pour finaliser votre dossier. Il est obligatoire d'avoir rempli tous les champs nécessaires, si il manque des informations les établissements ne pourront pas traiter correctement votre dossier.</p>
-					</div>
-					<div class="card-text text-phase-3">
-						<p>Il s'agît du moment décisif, vous reçevrez au fur et à mesure les différentes réponses. Il y a alors différentes possibilités :
+			<div class="section-content">
+				<ul class="menu-content">
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+				</ul>
+				<div class="container-content">
+					<p>
+						Bien que les étudiants viennent dans ce DUT pour apprendre, quelques connaissances sont nécessaires.
 						<ul>
-							<li>Vous êtes accepté : l'établissement vous offre une proposition d'admission, vous pouvez alors accepté ou refusé. Il est préférable de répondre assez rapidement.</li>
-							<li>Vous êtes en file d'attente : vous devez attendre que les places se libère, si des personnes refusent leur proposition vous avancerez dans la file. Il faut savoir être patient lors de cette phase.</li>
-							<li>Vous êtes refusé : vous n'avez pas de choix ici, vous n'aurez pas accès à cette formation.</li>
+							<li>Un bon niveau en mathématique (n'ayez pas peur, cela ne déterminera pas votre DUT, vous devrez simplement donner un peu plus d'efforts)</li>
+							<li>Une expression française correcte et une bonne maîtrise de l'anglais</li>
+							<li>Ouverture d'esprit, rigueur, autonomie</li>
+							<li>Logique, esprit d'analyse et de synthèse</li>
 						</ul>
-						</p>
-						<p>Ce délai ,très long, possède en réalité plusieurs phases, la procédure est suspendu lors des épreuves du baccalauréat et reprend le 29 juin jusqu'au 17 juillet. Surtout n'oubliez pas de refusé les propositions que vous ne désirez pas! Des étudiants attendent...</p>
-					</div>
-					<div class="card-text text-phase-4">
-						<p>Les inscriptions débutent dans les établissements, si vous avez déjà été accepté dans un d'eux vous pouvez commencez à vous inscrire.</p>
-					</div>
-					<div class="card-text text-phase-5">
-						<p>Vous pouvez formuler 10 nouveaux voeux complémentaires dans les établissements qui ont encore des places de disponibles.</p>
-						<br />
-						<p>Pour plus d'informations, rendez-vous sur le site de Parcoursup en cliquant <a href='https://www.parcoursup.fr/'>ici</a>.</p>
-					</div>
-				<h3>L'inscription au DUT Informatique de Dijon</h3>
-					<p>L'établissement examine votre dossier, si un équilibre en les différentes matières est présente, si votre comportement correspond à celui attendu,... Une fois que vous avez été accepté sur Parcoursup et que vous avez confirmé votre venu dans la formation, l'inscription débute en juillet et se déroule entièrement en ligne. Des dossiers seront mis à votre disposition. Des informations complémentaires ainsi que des papiers vous seront demandés. Pour vous inscrire vous aurez obligatoirement besoin de votre <a href='https://cvec.etudiant.gouv.fr/'>CVEC</a>, pour un coût de 90€. Les frais d'inscriptions s'élèvent ensuite à 170€.</p>
-			</div>
-			<div class="title-content">
-				<h2>L'inscription</h2>
+					</p>
+				</div>
 			</div>
 		</div>
-	</div>
+		<div class="content-part">
+			<div class="title-content-global">
+				<div class="title-content">
+					<div>
+						<h2>L'inscription</h2>
+					</div>
+				</div>
+			</div>
+			<div class="section-content">
+				<ul class="menu-content">
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+					<li>Lorem Ipsum</li>
+				</ul>
+				<div class="container-content">
+					<h3>Parcoursup</h3>
+						<p>Pour accéder au DUT Informatique vous devrez premièrement accéder à Parcoursup. Si vous êtes en établissement scolaire vos professeurs vous accompagneront dans la démarche d'inscription qui ouvre le 20 décembre.</p>
+					
+						<p>Le principe de Parcoursup est de formuler des voeux dans différents établissements qui analyseront par la suite votre dossier. Suite à votre inscription différentes démarches sont à effectuer..</p>
+						<div class="card-menu">
+							<div class="card-content card-phase-1" onclick="buttonAppearText(1)">
+								<span class="card-title">22 janvier - 12 mars : Formulation des voeux</span>
+							</div>
+							<div class="card-content card-phase-2" onclick="buttonAppearText(2)">
+								<span class="card-title">2 avril : Limite de la phase d'admission</span>
+							</div>
+							<div class="card-content card-phase-3" onclick="buttonAppearText(3)">
+								<span class="card-title">19 mai - 17 juillet</span>
+							</div>
+							<div class="card-content card-phase-4" onclick="buttonAppearText(4)">
+								<span class="card-title">Début juillet</span>
+							</div>
+							<div class="card-content card-phase-5" onclick="buttonAppearText(5)">
+								<span class="card-title">Fin juin - 11 septembre</span>
+							</div>
+						</div>
+
+						<div class="card-text text-phase-1">
+							<p>Cette période paraît longue mais vous devez impérativement réflechir aux différents voeux que vous allez faire, vous en avez une limite de 10. Vous n'êtes pas seuls à postuler, plus vous serez rapide et sûr de vous, mieux se déroulera vos démarches.</p>
+						</div>
+						<div class="card-text text-phase-2">
+							<p>Il s'agît de la date butoire pour finaliser votre dossier. Il est obligatoire d'avoir rempli tous les champs nécessaires, si il manque des informations les établissements ne pourront pas traiter correctement votre dossier.</p>
+						</div>
+						<div class="card-text text-phase-3">
+							<p>Il s'agît du moment décisif, vous reçevrez au fur et à mesure les différentes réponses. Il y a alors différentes possibilités :
+							<ul>
+								<li>Vous êtes accepté : l'établissement vous offre une proposition d'admission, vous pouvez alors accepté ou refusé. Il est préférable de répondre assez rapidement.</li>
+								<li>Vous êtes en file d'attente : vous devez attendre que les places se libère, si des personnes refusent leur proposition vous avancerez dans la file. Il faut savoir être patient lors de cette phase.</li>
+								<li>Vous êtes refusé : vous n'avez pas de choix ici, vous n'aurez pas accès à cette formation.</li>
+							</ul>
+							</p>
+							<p>Ce délai ,très long, possède en réalité plusieurs phases, la procédure est suspendu lors des épreuves du baccalauréat et reprend le 29 juin jusqu'au 17 juillet. Surtout n'oubliez pas de refusé les propositions que vous ne désirez pas! Des étudiants attendent...</p>
+						</div>
+						<div class="card-text text-phase-4">
+							<p>Les inscriptions débutent dans les établissements, si vous avez déjà été accepté dans un d'eux vous pouvez commencez à vous inscrire.</p>
+						</div>
+						<div class="card-text text-phase-5">
+							<p>Vous pouvez formuler 10 nouveaux voeux complémentaires dans les établissements qui ont encore des places de disponibles.</p>
+							<br />
+							<p>Pour plus d'informations, rendez-vous sur le site de Parcoursup en cliquant <a href='https://www.parcoursup.fr/'>ici</a>.</p>
+						</div>
+					<h3>L'inscription au DUT Informatique de Dijon</h3>
+						<p>L'établissement examine votre dossier, si un équilibre en les différentes matières est présente, si votre comportement correspond à celui attendu,... Une fois que vous avez été accepté sur Parcoursup et que vous avez confirmé votre venu dans la formation, l'inscription débute en juillet et se déroule entièrement en ligne. Des dossiers seront mis à votre disposition. Des informations complémentaires ainsi que des papiers vous seront demandés. Pour vous inscrire vous aurez obligatoirement besoin de votre <a href='https://cvec.etudiant.gouv.fr/'>CVEC</a>, pour un coût de 90€. Les frais d'inscriptions s'élèvent ensuite à 170€.</p>
+				</div>
+			</div>
+		</div>
 </main>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
